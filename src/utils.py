fc453cd02bd31a843dcf5133acd52cdaeff42648
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-12-18 15:37:19 -0300

    Update version 2.0 - 2021/12/18

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,28 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Import modules
+# Módulos importados
 from logging import warning, error
 from os import remove
-from os.path import expanduser, isfile
+from os.path import expanduser, isfile, realpath
 from shutil import copy
 
-# Import sources
+# Modulos integrados (src)
 from jsonTools import set_json, write_json
 
 
 ########################################################################################################################
 
 
-# Variables for define locals for autostart configuration
-orig = '/usr/share/applications/whats.desktop'
-dest = expanduser('~/.config/autostart')
-desk = dest + '/whats.desktop'
-
-
-# Define application icon
+# Define o ícone das aplicações.
 def set_icon(entry_icon=None):
-    icon = '/usr/share/pixmaps/whats.svg'
-    l_icon = '../appdata/whats.svg'
 
+    # Definição da localização dos ícones e de forma opcional, os ícones a serem usados
     if entry_icon is not None:
         icon = '/usr/share/whats/icon_status/' + entry_icon + '.png'
         l_icon = '../icon_status/' + entry_icon + '.png'
+    else:
+        icon = '/usr/share/pixmaps/whats.svg'
+        l_icon = '../appdata/whats.svg'
 
     try:
         with open(icon):
@@ -38,22 +33,39 @@ def set_icon(entry_icon=None):
             with open(l_icon):
                 return l_icon
         except Exception as msg:
-            # Exception for icon not found
+            # Caso nenhum ícone seja encontrado, vai sem ícone mesmo
             error('\033[31m %s \033[m', msg)
             return None
 
 
-# verify if exist the file whats.desktop in auto start folder
+# Verifica a existência do arquivo whats.desktop no diretório autostart.
 def set_desktop():
+    orig = '/usr/share/applications/whats.desktop'
+    dest = expanduser('~/.config/autostart')
+    desk = dest + '/whats.desktop'
+
     try:
-        if not isfile(desk) and set_json('AutoStart') == 'True':
-            with open(orig):
+        if not isfile(desk) and set_json('AutoStart'):
+            with open(orig):  # Antes da cópia, é preciso verificar a sua existência
                 copy(orig, dest)
-        elif isfile(desk) and set_json('AutoStart') == 'False':
+        elif isfile(desk) and not set_json('AutoStart'):
             remove(desk)
         return True
 
-    except Exception as msg:
+    except Exception as msg:  # Se o arquivo não existe, não tem porque ativar a opção autostart
         error('\033[31m %s \033[m', msg)
-        write_json('AutoStart', 'False')
+        write_json('AutoStart', False)
         return False
+
+
+# Busca por arquivos de áudio que servirão como tema das mensagens de notificação.
+def setSound(sound):
+    dirSound = '/usr/share/whats/sound/' + sound + '.mp3'
+    l_dirSound = realpath('../sound/' + sound + '.mp3')  # O realpath é nessesário para funcionar no QMediaPlayer
+
+    try:
+        with open(dirSound):
+            return dirSound  # Esse aqui não precisa realpath, pois é caminho absoluto
+    except Exception as msg:
+        warning('\033[33m %s.\033[32m Use a local sound folder...\033[m', msg)
+        return l_dirSound
