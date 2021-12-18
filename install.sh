385126ccc665f67da9dee329731746d513be6f43
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-12-18 20:34:23 -0300

    Update version 2.0 - 2021/12/18

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ install_root=${install_root:-""}
 
 set -e
 [ "$install_root" != "" ] && {
-  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,whats/icon_status},doc/whats-"$pkgver"}
+  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,whats/{icon_status,sound}},doc/whats-"$pkgver"}
 } || {
-  mkdir -p /usr/{share/whats/icon_status,doc/whats-"$pkgver"}
+  mkdir -p /usr/{share/whats/{icon_status,sound},doc/whats-"$pkgver"}
 }
 
 install -Dm 0644 appdata/whats.svg "$install_root"/usr/share/pixmaps
 install -Dm 0644 appdata/whats.desktop "$install_root"/usr/share/applications
 install -Dm 0644 icon_status/* "$install_root"/usr/share/whats/icon_status
+install -Dm 0644 sound/* "$install_root"/usr/share/whats/sound
 
 cp -a ChangeLog LICENSE README.md "$install_root"/usr/doc/whats-"$pkgver"
 cp -Tr src "$install_root"/usr/share/whats
