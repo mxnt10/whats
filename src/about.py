80966ecedad395ad546b6f1b804976b0487521fe
MauricioFerrari-NovaTrento <m10ferrari1200@gmail.com>
2021-12-19 11:22:10 -0300

    Update version 2.0 - 2021/12/18

--- src/about.py
+++ src/about.py
@@ -17,40 +17,31 @@ from version import __version__, __appname__, __pagename__
 class AboutDialog(QDialog):
     def __init__(self):
         super(AboutDialog, self).__init__()
-
-        # Definindo as propriedades da janela
         self.setWindowTitle('About ' + __appname__)
         self.setFixedSize(0, 0)
 
-        # Botão OK
-        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok)
-        self.buttonBox.accepted.connect(self.accept)
-
-        # Layout e Título
-        layout = QVBoxLayout()
+        # Título e Logo
         title = QLabel(__pagename__)
         font = title.font()
         font.setPointSize(20)
         title.setFont(font)
-        layout.addWidget(title)
-
-        # Definindo uma logo
         logo = QLabel()
         logo.setPixmap(QPixmap(set_icon('original')))
-        layout.addWidget(logo)
 
-        # Inserindo as mensagens de informação
+        # Criando o layout
+        layout = QVBoxLayout()
+        layout.addWidget(title)
+        layout.addWidget(logo)
         layout.addWidget(QLabel('Version ' + str(__version__) + '\n'))
         layout.addWidget(QLabel('Maintainer: Mauricio Ferrari'))
         layout.addWidget(QLabel('Contact: m10ferrari1200@gmail.com'))
         layout.addWidget(QLabel('License: GNU General Public License Version 3 (GLPv3)\n'))
 
-        # Definindo tudo no centro
+        # Definindo os widgets no centro
         for i in range(0, layout.count()):
             layout.itemAt(i).setAlignment(Qt.AlignHCenter)
 
-        # Adicionando o botão e criando o layout
-        layout.addWidget(self.buttonBox)
+        layout.addWidget(QDialogButtonBox(QDialogButtonBox.Ok, clicked=self.accept))
         self.setLayout(layout)
 
 
