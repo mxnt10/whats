30fbff9e80ec640a9d628d6085bc4c3a2fae43e7
mauricio <m10ferrari1200@gmail.com>
2022-02-20 11:33:50 -0300

    Performance optimization version 2.3 - 2022/02/20

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ install_root=${install_root:-""}
 
 set -e
 [ "$install_root" != "" ] && {
-  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,"$PRGNAM"/{icon_status,sound}},doc/"$PRGNAM"-"$VERSION"}
+  mkdir -p "$install_root"/usr/{bin,share/{applications,pixmaps,"$PRGNAM"/{icon_status,sound,translate}},doc/"$PRGNAM"-"$VERSION"}
 } || {
-  mkdir -p /usr/{share/"$PRGNAM"/{icon_status,sound},doc/"$PRGNAM"-"$VERSION"}
+  mkdir -p /usr/{share/"$PRGNAM"/{icon_status,sound,translate},doc/"$PRGNAM"-"$VERSION"}
 }
 
 install -Dm 0644 appdata/"$PRGNAM".svg "$install_root"/usr/share/pixmaps
 install -Dm 0644 appdata/"$PRGNAM".desktop "$install_root"/usr/share/applications
 install -Dm 0644 icon_status/* "$install_root"/usr/share/"$PRGNAM"/icon_status
+install -Dm 0644 translate/*.qm "$install_root"/usr/share/"$PRGNAM"/translate
 install -Dm 0644 sound/* "$install_root"/usr/share/"$PRGNAM"/sound
 
 cp -a ChangeLog LICENSE README.md "$install_root"/usr/doc/"$PRGNAM"-"$VERSION"
